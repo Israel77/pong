0ac3d19c876e8cb9866479bb32dfe8ab5faa3ff8
Israel Batista <israel.silvino@gmail.com>
2024-04-23 18:05:49 -0300

    Commit inicial

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <raylib.h>
+
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+int main() {
+        InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
+
+        CloseWindow();
+        return 0;
+}
