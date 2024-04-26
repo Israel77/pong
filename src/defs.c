2e5bcfeda67f948e78bdf9944bbff673f81e8d2b
Israel Batista <israel.silvino@gmail.com>
2024-04-26 19:26:38 -0300

    fix: Ajustar constantes

--- src/defs.c
+++ src/defs.c
@@ -9,7 +9,7 @@ const int PADDLE_OFFSET = 30;
 
 const int FPS = 60;
 
-const float BALL_SPEED = 7.0f;
+const float BALL_SPEED = 8.0f;
 const float BALL_RADIUS = 10.0f;
 
-const int PADDLE_SPEED = 10;
+const int PADDLE_SPEED = 5;
