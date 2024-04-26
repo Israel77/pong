b787f86d97be67a4654d08c0ddc511bd5ea18f57
Israel Batista <israel.silvino@gmail.com>
2024-04-26 15:26:43 -0300

    feat: Adicionar lógica de atualizar a raquete do jogador

--- src/main.c
+++ src/main.c
@@ -35,12 +35,27 @@ typedef struct {
 	int y;
 	int width;
 	int height;
+	int speed;
 } Paddle;
 
 void drawPaddle(Paddle *paddle)
 {
 	DrawRectangle(paddle->x, paddle->y, paddle->width, paddle->height,
 		      WHITE);
+}
+
+void updatePlayerPaddle(Paddle *paddle, int screenHeight)
+{
+	if (IsKeyDown(KEY_UP)) {
+		paddle->y -= paddle->speed;
+		if (paddle->y < 0)
+			paddle->y = 0;
+	}
+	if (IsKeyDown(KEY_DOWN)) {
+		paddle->y += paddle->speed;
+		if (paddle->y + paddle->height > screenHeight)
+			paddle->y = screenHeight - paddle->height;
+	}
 }
 
 int main()
@@ -51,33 +66,42 @@ int main()
 	const int PADDLE_WIDTH = 15;
 	const int PADDLE_HEIGHT = SCREEN_HEIGHT / 10;
 
+	const int FPS = 60;
+
+	const float BALL_SPEED = 7.0f;
+	const int PADDLE_SPEED = 10;
+
 	Ball ball = { .x = SCREEN_WIDTH / 2,
 		      .y = SCREEN_HEIGHT / 2,
-		      .radius = 15.0f,
-		      .speedX = 7.0f,
-		      .speedY = 7.0f,
+		      .radius = 20.0f,
+		      .speedX = BALL_SPEED,
+		      .speedY = BALL_SPEED,
 		      .color = WHITE };
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
-	SetTargetFPS(60);
+	SetTargetFPS(FPS);
 
 	Paddle leftPaddle = { .x = 30,
 			      .y = SCREEN_HEIGHT / 2 - PADDLE_HEIGHT / 2,
 			      .width = PADDLE_WIDTH,
-			      .height = PADDLE_HEIGHT };
+			      .height = PADDLE_HEIGHT,
+			      .speed = PADDLE_SPEED };
 
 	Paddle rightPaddle = { .x = SCREEN_WIDTH - 30 - PADDLE_WIDTH,
 			       .y = SCREEN_HEIGHT / 2 - PADDLE_HEIGHT / 2,
 			       .width = PADDLE_WIDTH,
-			       .height = PADDLE_HEIGHT };
+			       .height = PADDLE_HEIGHT,
+			       .speed = PADDLE_SPEED };
 
 	while (!WindowShouldClose()) {
 		ClearBackground(BLACK);
 		BeginDrawing();
 
+		updatePlayerPaddle(&rightPaddle, SCREEN_HEIGHT);
+		updateBall(&ball, SCREEN_WIDTH, SCREEN_HEIGHT);
+
 		drawPaddle(&leftPaddle);
 		drawPaddle(&rightPaddle);
-		updateBall(&ball, SCREEN_WIDTH, SCREEN_HEIGHT);
 		drawBall(&ball);
 
 		EndDrawing();
